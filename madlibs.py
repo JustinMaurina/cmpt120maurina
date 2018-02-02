cb69be38d6018a7c9297b11ca552ed73f1054f4c
JustinMaurina <Justin.Maurina1@marist.edu>
2018-02-02 17:39:12 -0500

    Added the madlibs activity.

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+#Mad Libs
+def main():
+    Noun=input("Enter a noun: ")
+    Verb= input("Enter a verb: ")
+    Adjective=input("Enter an adjective: ")
+    Place=input("Enter a place: ")
+    print("My Madlibs.")
+    print ("Take your", Adjective, Noun, "and", Verb, "it at the", Place)
+main()
