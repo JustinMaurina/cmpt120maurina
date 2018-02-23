25adbadd28c528110a3a49b0765eded9c7679c52
JustinMaurina <Justin.Maurina1@marist.edu>
2018-02-23 11:05:24 -0500

    First step in lab 4.

---
+++ Credientials.py
@@ -0,0 +1,23 @@
+# CMPT 120 Intro to Programming
+# Lab 5- Strings and Functions
+# Author- Justin Maurina
+# Created: 2018-02-23
+
+import math
+def main():
+# get user's first and last names
+    first = input("Enter your first name: ")
+    last = input("Enter your last name: ")
+# Todo modify this to create a Marist styled username
+    uname = first+Ju "." + last
+# ask user to set a new password
+    passwd = input ("Set a new password: ")
+# Todo modify this to make sure the password has at least 8 characters
+    if len(passwd) >8: True
+    print("Fool of a Took! That password is feeble!")
+    passwd = input("Create a new password: ")
+    if len (passwd) <8: False
+    print("The force is strong in this one...")
+    print("Account configured. Your new email address is",
+    uname + "@marist.edu")
+main()
