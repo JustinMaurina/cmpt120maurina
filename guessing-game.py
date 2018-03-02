ae2bb50d77bd0cbae2793bbfe75fcc85ff7045f8
JustinMaurina <35709446+JustinMaurina@users.noreply.github.com>
2018-03-02 12:53:55 -0500

    Add files via upload
    Usability improvements to the guessing game, resolves #1 and #2.

--- guessing-game.py
+++ guessing-game.py
@@ -1,12 +1,18 @@
 # This is a guessing game.
 
-answer = 'Kangaroo'
+answer1 = 'kangaroo'
+answer2 = 'Kangaroo'
+answer3 = 'KANGAROO'
 
 while 1==1:
     print("The program is thinking of an animal.")
     print("Guess the animal.")
-    if input() == answer:
+    if input() == answer1 or answer2 or answer3:
         print("That was the animal. Good job!")
         break
     else:
         print("That was not the animal. Please try again!")
+    if input() == 'quit':
+        print("Thanks for plaiying.")
+        break
+        
