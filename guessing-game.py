374765f84accf8b4a024271de3e89568071a2ba6
JustinMaurina <35709446+JustinMaurina@users.noreply.github.com>
2018-03-02 17:56:29 -0500

    Usability improvement to the guessing game, resolves issues #1 and #2.

--- guessing-game.py
+++ guessing-game.py
@@ -1,18 +1,31 @@
 # This is a guessing game.
 
 answer1 = 'kangaroo'
-answer2 = 'Kangaroo'
-answer3 = 'KANGAROO'
+answer2 = 'KANGAROO'
+answer3 = 'Kangaroo'
+
 
 while 1==1:
     print("The program is thinking of an animal.")
     print("Guess the animal.")
+
     if input() == answer1 or answer2 or answer3:
         print("That was the animal. Good job!")
+        print("Do you like this animal? Answer with either yes or no.")
+        input() == 'Yes'
+        print("Me too, it's my favorite!")
+        input() == 'No'
+        print("How? It is so cute.")
         break
     else:
         print("That was not the animal. Please try again!")
-    if input() == 'quit':
-        print("Thanks for plaiying.")
+
+    var == 'q'
+    while input() == var or 'quit':
+        print("Thanks for playing.")
         break
+
+    
+
+    
         
